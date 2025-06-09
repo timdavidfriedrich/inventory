6a2c99fa41fe3548ee26217f6eef2cd4f47d8abb
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2025-06-09 15:52:17 +0200

    [CHANGE] rebuild

--- lib/service_locator.config.dart
+++ lib/service_locator.config.dart
@@ -20,12 +20,6 @@ import 'package:inventory/core/data/repositories/item_repository_impl.dart'
     as _i945;
 import 'package:inventory/core/domain/repositories/item_repository.dart'
     as _i1052;
-import 'package:inventory/core/domain/usecases/item/get_all_items_use_case.dart'
-    as _i16;
-import 'package:inventory/core/domain/usecases/item/save_item_use_case.dart'
-    as _i63;
-import 'package:inventory/features/overview/presentation/overview_bloc.dart'
-    as _i328;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -47,14 +41,6 @@ extension GetItInjectableX on _i174.GetIt {
             localDatabase: gh<_i965.LocalDatabase>()));
     gh.lazySingleton<_i1052.ItemRepository>(() => _i945.ItemRepositoryImpl(
         localDataSource: gh<_i834.LocalDatabaseDataSource>()));
-    gh.factory<_i16.GetAllItemsUseCase>(
-        () => _i16.GetAllItemsUseCase(repository: gh<_i1052.ItemRepository>()));
-    gh.factory<_i63.SaveItemUseCase>(
-        () => _i63.SaveItemUseCase(repository: gh<_i1052.ItemRepository>()));
-    gh.factory<_i328.OverviewBloc>(() => _i328.OverviewBloc(
-          getAllItemsUseCase: gh<_i16.GetAllItemsUseCase>(),
-          saveItemUseCase: gh<_i63.SaveItemUseCase>(),
-        ));
     return this;
   }
 }
