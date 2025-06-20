ba2519faa3f131a6d0548416e3ab4a31c737365b
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2025-06-20 15:31:22 +0200

    [ADD] TextField and Text theming

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:inventory/src/core/presentation/dimensions.dart';
 
 class AppTheme {
   const AppTheme._();
 
   static ThemeData get light {
-    return _theme(Brightness.light);
+    return _theme(
+      ColorScheme.fromSeed(
+        seedColor: Colors.blue,
+        brightness: Brightness.light,
+      ),
+    );
   }
 
   static ThemeData get dark {
-    return _theme(Brightness.dark);
+    return _theme(
+      ColorScheme.fromSeed(
+        seedColor: Colors.blue,
+        brightness: Brightness.dark,
+      ),
+    );
   }
 
-  static ThemeData _theme(Brightness brightness) {
+  static ThemeData _theme(ColorScheme colorScheme) {
     return ThemeData(
-      brightness: brightness,
-      colorScheme: ColorScheme.fromSeed(
-        seedColor: Colors.blue,
-        brightness: brightness,
-      ),
+      brightness: colorScheme.brightness,
+      colorScheme: colorScheme,
       cupertinoOverrideTheme: CupertinoThemeData(
-        brightness: brightness,
+        brightness: colorScheme.brightness,
         barBackgroundColor: Colors.transparent,
       ),
       pageTransitionsTheme: const PageTransitionsTheme(
@@ -28,6 +36,65 @@ class AppTheme {
           TargetPlatform.android: PredictiveBackPageTransitionsBuilder(),
           TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
         },
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: colorScheme.surfaceContainer,
+        hoverColor: colorScheme.surfaceContainerLowest,
+        focusColor: colorScheme.primary,
+        iconColor: colorScheme.onSurfaceVariant,
+        suffixIconColor: colorScheme.onSurfaceVariant,
+        prefixIconColor: colorScheme.onSurfaceVariant,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(Dimensions.semiSmallBorderRadius),
+          borderSide: BorderSide(
+            color: colorScheme.outline,
+            width: Dimensions.mediumBorderWidth,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(Dimensions.semiSmallBorderRadius),
+          borderSide: BorderSide(
+            color: colorScheme.primary,
+            width: Dimensions.mediumBorderWidth,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(Dimensions.semiSmallBorderRadius),
+          borderSide: BorderSide(
+            color: colorScheme.outline,
+            width: Dimensions.mediumBorderWidth,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(Dimensions.semiSmallBorderRadius),
+          borderSide: BorderSide(
+            color: colorScheme.errorContainer,
+            width: Dimensions.mediumBorderWidth,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(Dimensions.semiSmallBorderRadius),
+          borderSide: BorderSide(
+            color: colorScheme.error,
+            width: Dimensions.mediumBorderWidth,
+          ),
+        ),
+        disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(Dimensions.semiSmallBorderRadius),
+          borderSide: BorderSide(
+            color: colorScheme.outlineVariant,
+            width: Dimensions.mediumBorderWidth,
+          ),
+        ),
+      ),
+      textTheme: TextTheme(
+        displayLarge: TextStyle(fontFamily: "Khuja"),
+        displayMedium: TextStyle(fontFamily: "Khuja"),
+        displaySmall: TextStyle(fontFamily: "Khuja"),
+        titleLarge: TextStyle(fontFamily: "Khuja"),
+        titleMedium: TextStyle(fontFamily: "Khuja"),
+        titleSmall: TextStyle(fontFamily: "Khuja"),
       ),
     );
   }
