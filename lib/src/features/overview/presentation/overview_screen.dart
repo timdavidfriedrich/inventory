6029095af352ae5e8220a02a202a4e6743a53da7
Tim David Friedrich <tim_david.friedrich@stud.htwk-leipzig.de>
2025-11-05 09:06:48 +0100

    [ADD] overview page title (different feature, oops)

--- lib/src/features/overview/presentation/overview_screen.dart
+++ lib/src/features/overview/presentation/overview_screen.dart
@@ -88,6 +88,7 @@ class _MaterialAppBar extends StatelessWidget implements PreferredSizeWidget {
         icon: Icon(AppIcons.settings(context)),
         onPressed: () => SettingsRoute().push(context),
       ),
+      title: Text(context.s.screen_overview),
       actions: [
         IconButton(
           icon: Icon(AppIcons.archive(context)),
@@ -142,6 +143,7 @@ class _CupertinoAppBar extends StatelessWidget implements PreferredSizeWidget {
           AppIcons.more(context),
         ),
       ),
+      middle: Text(context.s.screen_overview),
       trailing: GestureDetector(
         onTap: () => CaptureFlowCoordinator.startCaptureFlow(context),
         child: Icon(
